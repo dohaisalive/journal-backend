a0e8832bf839038eec79ab4a855ed9d4500ee45c
Nora Khalid <nalobaidi_@hotmail.com>
2022-07-03 23:40:41 +0300

    Create models

---
+++ DB/models/Notification.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require("mongoose");
+
+const NotificationSchema = new Schema({
+sender:{type:Schema.Types.ObjectId,ref:"User"},
+receiver:{type:Schema.Types.ObjectId,ref:"User"},
+type:{type:String},
+});
+
+
+module.exports = model("Notification", NotificationSchema);
